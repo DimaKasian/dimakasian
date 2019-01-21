974af51071ab0b2fab005b701ae1e8df6d767c84
Dima Kasian <dimakasian@gmail.com>
2019-01-21 19:31:12 +0200

    add two section

--- examFinal/app/js/script.js
+++ examFinal/app/js/script.js
@@ -1,2 +1,2 @@
 "use strict";
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNjcmlwdC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSIsImZpbGUiOiJzY3JpcHQuanMiLCJzb3VyY2VzQ29udGVudCI6WyJcInVzZSBzdHJpY3RcIjsiXX0=
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsInNvdXJjZXNDb250ZW50IjpbXSwiZmlsZSI6InNjcmlwdC5qcyJ9
--- examFinal/gulpfile.js
+++ examFinal/gulpfile.js
@@ -1,26 +1,42 @@
 'use strict';
 
-const   gulp = require('gulp'),
-autoprefixer = require('gulp-autoprefixer'),
-notify = require('gulp-notify'),
-sass = require('gulp-sass'),
-sourcemaps = require('gulp-sourcemaps'),
-clean = require('gulp-clean'),
-babel = require('gulp-babel'),
-uglify = require('gulp-uglify'),
+const gulp = require('gulp'),
+$ = require('gulp-load-plugins')(),
 browserSync = require('browser-sync').create();
+// autoprefixer = require('gulp-autoprefixer'),
+// notify = require('gulp-notify'),
+// sass = require('gulp-sass'),
+// sourcemaps = require('gulp-sourcemaps'),
+// clean = require('gulp-clean'),
+// babel = require('gulp-babel'),
+// uglify = require('gulp-uglify'),
+// imgmin = require('gulp-imagemin'),
+// htmlmin = require('gulp-htmlmin');
 
 
 gulp.task('sass', () => {
   return setTimeout(() => {
     return gulp.src('src/scss/**/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'compressed'}).on('error', notify.onError("SASS-Error: <%= error.message %>")))
-    .pipe(autoprefixer({
+    .pipe($.sourcemaps.init())
+    .pipe($.sass().on('error', $.notify.onError("SASS-Error: <%= error.message %>")))
+    .pipe($.autoprefixer({
      browsers: ['last 2 versions'],
      cascade: false
    }))
-    .pipe(sourcemaps.write())
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.stream());
+  }, 500);
+});
+
+gulp.task('sass:prod', () => {
+  return setTimeout(() => {
+    return gulp.src('src/scss/**/*.scss')
+    .pipe($.sass({outputStyle: 'compressed'}).on('error', $.notify.onError("SASS-Error: <%= error.message %>")))
+    .pipe($.autoprefixer({
+     browsers: ['last 2 versions'],
+     cascade: false
+   }))
     .pipe(gulp.dest('app/css'))
     .pipe(browserSync.stream());
   }, 500);
@@ -32,22 +48,50 @@ gulp.task('html', () => {
  .pipe(browserSync.stream());
 });
 
+gulp.task('html:prod', () => {
+ return gulp.src('src/index.html')
+ .pipe($.htmlmin({ collapseWhitespace: true }))
+ .pipe(gulp.dest('app/'))
+ .pipe(browserSync.stream());
+});
+
 gulp.task('js', () => {
  return gulp.src('src/js/**/*.js')
- .pipe(sourcemaps.init())
- .pipe(babel({
-            presets: ['@babel/env']
-        }).on('error', notify.onError("JS-babel-Error: <%= error.message %>")))
- .pipe(uglify().on('error', notify.onError("JS-uglify-Error: <%= error.message %>")))
- .pipe(sourcemaps.write())
+ .pipe($.sourcemaps.init())
+ .pipe($.babel({
+  presets: ['@babel/env']
+}).on('error', $.notify.onError("JS-babel-Error: <%= error.message %>")))
+ .pipe($.sourcemaps.write())
  .pipe(gulp.dest('app/js'))
  .pipe(browserSync.stream());
 });
 
+gulp.task('js:prod', () => {
+ return gulp.src('src/js/**/*.js')
+ .pipe($.babel({
+  presets: ['@babel/env']
+}).on('error', $.notify.onError("JS-babel-Error: <%= error.message %>")))
+ .pipe($.uglify().on('error', $.notify.onError("JS-uglify-Error: <%= error.message %>")))
+ .pipe(gulp.dest('app/js'))
+ .pipe(browserSync.stream());
+});
+
+gulp.task('img:dev', () => {
+ return gulp.src('src/img/**/*.*')
+ .pipe(gulp.dest('app/img'))
+ .pipe(browserSync.stream());
+});
 
 gulp.task('img', () => {
  return gulp.src('src/img/**/*.*')
+ .pipe($.imagemin())
  .pipe(gulp.dest('app/img'))
+ .pipe(browserSync.stream());
+});
+
+gulp.task('libs', () => {
+ return gulp.src('src/libs/**/*.*')
+ .pipe(gulp.dest('app/libs'))
  .pipe(browserSync.stream());
 });
 
@@ -61,11 +105,11 @@ gulp.task('watch', () => {
  gulp.watch('src/scss/**/*.scss', ['sass']),
  gulp.watch('src/index.html',['html']),
  gulp.watch('src/js/**/*.js',['js']),
-  gulp.watch('src/img/**/*.*',['img']),
+ gulp.watch('src/img/**/*.*',['img']),
  gulp.watch('src/fonts/**/*.*',['fonts'])
 });
 
-gulp.task('connect', function() {
+gulp.task('connect', () => {
  browserSync.init({
    server: {
      baseDir: "app",
@@ -74,16 +118,18 @@ gulp.task('connect', function() {
  });
 });
 
-gulp.task('clean', function () {
+gulp.task('clean', () => {
   return gulp.src('app')
-  .pipe(clean({
+  .pipe($.clean({
     force: true,
     read: false
   }));
 });
 
-gulp.task('developing', ['watch', 'html', 'js', 'img', 'fonts', 'sass', 'connect']);
+gulp.task('dev', ['watch', 'html', 'js', 'libs', 'img', 'fonts', 'sass', 'connect']);
+
+gulp.task('prod', ['watch', 'html:prod', 'js:prod', 'libs', 'img:prod', 'fonts', 'sass:prod', 'connect']);
 
 gulp.task('default', ['clean'], () => {
-  gulp.start('developing');
+  gulp.start('dev');
 });
