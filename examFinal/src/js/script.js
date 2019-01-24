c86f64f200e5c7a1937b7495ef8e3a93ed9b434d
Dima Kasian <dimakasian@gmail.com>
2019-01-24 12:31:47 +0200

    adaptive error

---
+++ examFinal/src/js/script.js
@@ -0,0 +1,10 @@
+
+window.onscroll = () => {
+	let scrolled = window.pageYOffset || document.documentElement.scrollTop;
+	const header = document.getElementById('header');
+	if(scrolled < 50) {
+		header.classList.remove('scroll');
+	} else {
+		header.classList.add('scroll');
+	}
+}
