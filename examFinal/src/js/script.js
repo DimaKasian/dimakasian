c0fc8a58f30f187c3caddc52f0dcbecc1b3849ca
Dima Kasian <dimakasian@gmail.com>
2019-01-25 12:25:02 +0200

    add all responsive

--- examFinal/src/js/script.js
+++ examFinal/src/js/script.js
@@ -9,3 +9,26 @@ window.onscroll = () => {
 	}
 }
 
+
+
+// function initMap() {
+// // Координаты центра на карте. Широта: 56.2928515, Долгота: 43.7866641
+// var centerLatLng = new google.maps.LatLng(49.065672, 33.414046);
+// // Обязательные опции с которыми будет проинициализированна карта
+// var mapOptions = {
+// center: centerLatLng, // Координаты центра мы берем из переменной centerLatLng
+// zoom: 8               // Зум по умолчанию. Возможные значения от 0 до 21
+// };
+// // Создаем карту внутри элемента #map
+// var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+// }
+// // Ждем полной загрузки страницы, после этого запускаем initMap()
+// google.maps.event.addDomListener(window, "load", initMap);
+
+// function initMap() {
+// 	directionsService = new google.maps.DirectionsService();
+// 	directionsDisplay = new google.maps.DirectionsRenderer();
+// 	const krem = {lat: 49.062785, lng: 33.4156805};
+// 	map = new google.maps.Map(document.getElementById('map'), {zoom: 7, center: krem});
+// 	directionsDisplay.setMap(map);
+// }
