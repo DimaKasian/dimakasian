ee397004615e8c8eeb795d60f5a9bb7c84b8fe9c
Dima Kasian <dimakasian@gmail.com>
2019-01-27 09:15:20 +0200

    slider

--- examFinal/src/js/script.js
+++ examFinal/src/js/script.js
@@ -11,30 +11,48 @@ window.onscroll = () => {
 
 
 
-// function initMap() {
-// // Координаты центра на карте. Широта: 56.2928515, Долгота: 43.7866641
-// var centerLatLng = new google.maps.LatLng(49.065672, 33.414046);
-// // Обязательные опции с которыми будет проинициализированна карта
-// var mapOptions = {
-// center: centerLatLng, // Координаты центра мы берем из переменной centerLatLng
-// zoom: 8               // Зум по умолчанию. Возможные значения от 0 до 21
-// };
-// // Создаем карту внутри элемента #map
-// var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-// }
-// // Ждем полной загрузки страницы, после этого запускаем initMap()
-// google.maps.event.addDomListener(window, "load", initMap);
+var map;
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 49.062785, lng: 33.4156805},
+		zoom: 12,
+		disableDefaultUI: true,
+	});
 
-// function initMap() {
-// 	directionsService = new google.maps.DirectionsService();
-// 	directionsDisplay = new google.maps.DirectionsRenderer();
-// 	const krem = {lat: 49.062785, lng: 33.4156805};
-// 	map = new google.maps.Map(document.getElementById('map'), {zoom: 7, center: krem});
-// 	directionsDisplay.setMap(map);
-// }
-
-//Carousel
-
-//action on right button
+}
 
 
+
+$(document).ready(function(){
+	$('.slider').slick({
+		infinite: true,
+		speed: 300,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		responsive: [
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1,
+				infinite: true,
+			}
+		},
+		{
+			breakpoint: 576,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
+
+		]
+	});
+});
